c3962d868705a437757232fc6e684d37696bded4
PierroD <lightningbird69@gmail.com>
2020-03-27 21:24:06 +0100

    UnitOfWork et Repository

---
+++ GSBrevet/Repositories/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+using System;
+using BackEndGSBrevet;
+
+namespace BackEndGSBrevet.Repositories.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+       // ITicketRepository Tickets { get; }
+        
+    }
+}
